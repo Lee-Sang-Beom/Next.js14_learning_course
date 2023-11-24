0753cabdbee3b5237c207805becb639f479e678f
이상범 <tkdqja975@naver.com>
2023-11-24 10:44:07 +0900

    chapter4 완료

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/app/ui/global.css";
-import { inter, lusitana } from "./ui/font";
+import { inter, lusitana } from "@/app/ui/font";
 
 export default function RootLayout({
   children,
@@ -8,7 +8,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* 여기! */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
