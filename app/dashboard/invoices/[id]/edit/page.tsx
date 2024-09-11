c46a82d48d6824bb17d768ab776feccae21aae5d
이상범 <tkdqja975@naver.com>
2024-09-11 11:58:37 +0900

    update an invoice 추가

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  // Promise.all을 사용하여 인보이스와 고객 데이터를 동시에 가져옴
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id), // 특정 인보이스 데이터를 가져옴
+    fetchCustomers(), // 고객 데이터를 가져옴
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,11 +6,11 @@ import { redirect } from "next/navigation";
 import { z } from "zod";
 
 const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
-  date: z.string(),
+  id: z.string(), // id는 문자열이어야 한다.
+  customerId: z.string(), // customerId는 문자열이어야 한다.
+  amount: z.coerce.number(), // amount는 coerce에 의해 number 타입으로 강제변환한다. ("100" -> 100)
+  status: z.enum(["pending", "paid"]), // status는 여럭형으로 정의되어야 한다. (2개중 하나여야 함)
+  date: z.string(), // date는 문자열이어야 한다.
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
@@ -32,4 +32,27 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices"); // add code
+}
+
+// Zod를 사용하여 예상되는 타입 정의
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  // 클라이언트 캐시 무효화 및 서버 요청 갱신
+  revalidatePath("/dashboard/invoices");
+  // 업데이트 후 인보이스 페이지로 리다이렉트
+  redirect("/dashboard/invoices");
 }
