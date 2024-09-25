5b202013d4a46492d51dfefa80b58376d31e17a2
이상범 <tkdqja975@naver.com>
2024-09-25 16:02:16 +0900

    apply : next.js learning course 13 : handling err

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -10,6 +11,8 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id), // 특정 인보이스 데이터를 가져옴
     fetchCustomers(), // 고객 데이터를 가져옴
   ]);
+
+  if (!invoice) notFound();
 
   return (
     <main>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (e) {
+    return {
+      message: "DB Error : Faileed to Create Invoice",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices"); // add code
@@ -45,11 +51,17 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (e) {
+    return {
+      message: "DB Error : Faileed to Update Invoice",
+    };
+  }
 
   // 클라이언트 캐시 무효화 및 서버 요청 갱신
   revalidatePath("/dashboard/invoices");
@@ -58,6 +70,11 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath("/dashboard/invoices");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+    return { message: "Deleted Invoice." };
+  } catch (error) {
+    return { message: "Database Error: Failed to Delete Invoice." };
+  }
 }
