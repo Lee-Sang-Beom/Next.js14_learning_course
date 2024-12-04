aaf34d3f655d0c381329056817bdbb2484375e9b
이상범 <tkdqja975@naver.com>
2024-12-04 09:18:26 +0900

    Next.js 학습코스-14 Chapter 완료 (practice까지)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -74,13 +74,24 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 // Zod를 사용하여 예상되는 타입 정의
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
